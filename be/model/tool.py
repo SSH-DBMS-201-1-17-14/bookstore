951404dc57bf035fa0e223d299f11f0558123cba
shy <shyhaoyang@126.com>
2021-01-11 20:57:41 +0800

    Merge branch 'main' of https://github.com/SSH-DBMS-201-1-17-14/bookstore into main
     Conflicts:
    	be/model/db_conn.py
    	be/model/error.py

--- be/model/tool.py
+++ be/model/tool.py
@@ -4,8 +4,7 @@ import psycopg2
 def cancel_order_tool(conn,order_id):
     cursor=conn.cursor()
     cursor.execute("select store_id from \"new_order\" where order_id=(%s)", (order_id,))
-
-    logging.log(logging.CRITICAL, "*****"+order_id+"*******")
+    # logging.log(logging.CRITICAL, "*****"+order_id+"*******")
     store_id = cursor.fetchone()[0]
     cursor.execute("select book_id,count from \"new_order_detail\" where order_id=(%s)", (order_id,))
     rows = cursor.fetchall()
