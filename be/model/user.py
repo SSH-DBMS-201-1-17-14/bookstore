0758740d7d23f6d6e3f8a6fe799e31f47837e30d
qifeng1128 <59163751+qifeng1128@users.noreply.github.com>
2021-01-12 14:15:59 +0800

    Update user.py

--- be/model/user.py
+++ be/model/user.py
@@ -199,60 +199,6 @@ class User(db_conn.DBConn):
             return 530, "{}".format(str(e))
         return 200, "ok"
 
-    # 对一个字符串进行分词，获得词项对应的列表
-    def split_user_input(self, string):
-        new_string = re.sub(r'[^\w\s]', '', string)  # 只保留中文和英文字符，去掉符号
-        terms = jieba.lcut(new_string)  # 分词 得到分词列表
-        # 去除为空的
-        while ' ' in terms:
-            terms.remove(' ')
-        while '\n' in terms:
-            terms.remove('\n')
-        return terms
-
-    # 根据关键词列表获得 book_id 对应 其包含关键词个数 的字典
-    def find_ids(self, terms, index_dic):  # ['三毛','的','三毛']
-        term_in_id = {}
-        for term in terms:
-            if term in index_dic.keys():
-                value_ids = index_dic[term]
-                for id in value_ids:
-                    if id in term_in_id.keys():
-                        term_in_id[id] = term_in_id[id] + 1
-                    else:
-                        term_in_id[id] = 1
-        return term_in_id
-
-    # 传入 book_id 对应 其包含关键词个数 的字典以及 book_id 对应 分词后词项长度 的字典
-    # 对每个 book_id 的重要程度打分并且降序排序，返回对应页数的 book_id 列表
-    def sort_id_importance_pagek(self, term_in_id, freqdic, pagek):
-        importance_score = {}
-        # 对id
-        for id_num in term_in_id.keys():
-            word_num = freqdic[id_num]  # 得到id对应字符串本身分词个数
-            score = term_in_id[id_num] / word_num * 1.0  # 得分
-            importance_score[id_num] = score  # 得到所有排序
-        importance_score = sorted(importance_score.items(), key=lambda d: d[1], reverse=True)  # 由分数的从高到低进行排序
-        # 因为最后一页不一定能显10个，判断一下能分几页
-        id_n = len(importance_score)
-        # print('.........id_n......... = %d'%id_n)
-        pages_num = ceil(id_n / 10 * 1.0)  # 共有pages_num
-        # print('.........pages_num......... = %d'%pages_num)
-        # 输入页码超过最大页数,输出空列表
-        if pagek > pages_num:
-            return []
-        # 输入页码小于等于最大页数
-        # 分页 第k页：下标（从0开始）  ： 10*(k-1) 到 k*10-1
-        start = 10 * (pagek - 1)
-        end = pagek * 10 - 1
-        # 如果是最后一页,不一定能输10个，只能输出到最后一个
-        if pagek == pages_num:
-            end = id_n - 1
-        id_l = []  # 最后输出的id
-        for i in range(start, end + 1):
-            id_l.append(importance_score[i][0])
-        return id_l
-
     def store_search_title(self, user_id: str, store_id: str, search_info: str, page: int):
         try:
             # 首先，判断 store 表中是否存在 store_id，可能存在用户建店铺但未上架新书的情况
